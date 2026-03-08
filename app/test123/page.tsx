9991dca126f623f6f0cc6a4a99d1fe1b9bc0680a
BerkAkn <berkakin700@gmail.com>
2026-03-08 20:45:57 +0300

    add test123 route

---
+++ app/test123/page.tsx
@@ -0,0 +1,7 @@
+export default function Test123Page() {
+  return (
+    <main style={{ minHeight: "100vh", padding: 40, background: "#151233", color: "white" }}>
+      <h1>test123 works</h1>
+    </main>
+  );
+}
